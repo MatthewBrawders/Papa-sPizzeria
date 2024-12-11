dbe4b84102aba4e3f2108979d6d326adefe26141
Matthew Brawders <mbraw003@xe-10.cs.ucr.edu>
2024-12-11 12:27:23 -0800

    Added some trigggers (i'm triggered)

---
+++ cs166_project_phase3/sql/src/create_triggers.sql
@@ -0,0 +1,68 @@
+--New User Triggers
+CREATE TRIGGER set_default_user_attributes
+AFTER INSERT ON Users
+FOR EACH ROW
+BEGIN
+    UPDATE Users
+    SET role = 'customer', favoriteItem = NULL
+    WHERE userID = NEW.userID;
+END;
+
+--Place Order Trigger
+
+
+--Update Order Status
+CREATE TRIGGER update_order_status_timestamp
+AFTER UPDATE ON FoodOrder
+FOR EACH ROW
+WHEN OLD.orderStatus != NEW.orderStatus
+BEGIN
+    UPDATE FoodOrder
+    SET orderTimestamp = NOW()
+    WHERE orderID = NEW.orderID;
+END;
+
+--See Order History
+CREATE TRIGGER update_recent_orders
+AFTER INSERT ON FoodOrder
+FOR EACH ROW
+BEGIN
+    INSERT INTO recentOrders (userID, orderID, orderTimestamp)
+    VALUES (NEW.userID, NEW.orderID, NEW.orderTimestamp);
+
+    DELETE FROM recentOrders
+    WHERE userID = NEW.userID
+      AND orderTimestamp NOT IN (
+          SELECT orderTimestamp
+          FROM recentOrders
+          WHERE userID = NEW.userID
+          ORDER BY orderTimestamp DESC
+          LIMIT 5
+      );
+END;
+
+
+--If Exists Checks
+    --Store
+CREATE TRIGGER validate_store_on_order
+BEFORE INSERT ON FoodOrder
+FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Store WHERE storeID = NEW.storeID) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid store ID.';
+    END IF;
+END;
+
+    --Menu
+CREATE TRIGGER prevent_unavailable_items
+BEFORE INSERT ON ItemsInOrder
+FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Menu WHERE itemName = NEW.itemName AND available = 1) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Item not available.';
+    END IF;
+END;
+
+
